a76a90060f188f92e838fe5b00ddf63dd579b5bb
AlecMangkharakhili <alecmang097@gmail.com>
2018-03-30 19:45:25 -0500

    I think I figured out how basic routing works

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/routetest', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
 module.exports = router;
