cf3427dc0e2833379fa382eb7dc5801eea914051
AlecMangkharakhili <alecmang097@gmail.com>
2018-04-10 22:07:58 -0500

    Committing to fetch all branches

--- app.js
+++ app.js
@@ -41,12 +41,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', loginRouter);
-app.use('/routetest', usersRouter);
-app.use('/adduser', addUserRouter);
 
 //Gets login from login page
 app.get('/', (req, res) => {
-  next(usersRouter);
 });
 
 // catch 404 and forward to error handler
--- routes/login.js
+++ routes/login.js
@@ -7,4 +7,8 @@ router.get('/', function(req, res, next) {
   res.render('login'); //RENDER USES THE PUG TEMPLATES
 });
 
+router.get('/nextroute', function(req, res, next) {
+  res.render('adduser'); //RENDER USES THE PUG TEMPLATES
+});
+
 module.exports = router;
