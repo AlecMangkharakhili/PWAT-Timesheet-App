3953240f3e3af8010d46244472952c63b86f09dc
AlecMangkharakhili <alecmang097@gmail.com>
2018-04-13 16:30:46 -0500

    Deleted unnecessary javascript files and started moving all routes to index

--- app.js
+++ app.js
@@ -7,50 +7,15 @@ const mysql = require('mysql');
 const expressValidator = require('express-validator');
 const bcrypt = require('bcrypt');
 const sanitizer = require('express-validator/filter');
-const session = require('client-sessions');
 require('dotenv').config();
 
-// Initializes connection to database using environment variables
-var connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-
-connection.connect((err) => {
-  if(err){
-    throw err;
-  }
-  console.log('Connection successful');
-});
-
-// Routes
-var home = require('./routes/home');
-var redirectToLogin = require('./routes/redirectToLogin');
-var login = require('./routes/login');
-var addUser = require('./routes/adduser');
-
 var app = express();
-
-// Global Vars
-app.use((req, res, next) => {
-  res.locals.errors = null;
-  next();
-});
 
 // Express middleware
   // Middleware for form validation
 app.use(expressValidator());
 
-  // Middle ware for sessions and cookies
-app.use(session({
-  cookieName: 'session',
-  secret: 'changethislater',
-  duration: 30 * 60 * 1000,
-  activeDuration: 30 * 60 * 1000
-}));
+var index = require('./routes/index');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -62,75 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/login', login);
-app.use('/', redirectToLogin);
-app.use('/adduser', addUser);
-app.use('/home', home);
-
-// Login authentication/render
-app.post('/login', (req, res) => {
-  let checkLogin = {
-    username: req.body.username,
-    password: req.body.password
-  };
-  let query = connection.query('SELECT password FROM users WHERE username = ?', [checkLogin.username], (err, results) => {
-    if(err){
-      console.log(err);
-    }
-    else{
-      // Checks if the user is in the database
-      if(results != ''){
-        if(bcrypt.compareSync(req.body.password, results[0].password)){
-          res.redirect('/home');
-          req.session.user = req.body.username;
-        }
-        else{
-          res.render('login', {errors: 'Invalid username or password'});
-        }
-      }
-      else{
-        res.render('login', {errors: 'Invalid username or password'});
-      }
-    }
-  });
-});
-
-// Pulls information from create adduser page and inserts it into the DB
-// POSTS user information into the database
-app.post('/users/add', (req, res) => {
-  
-  // Form validation
-  req.checkBody('firstname', 'First name is required').notEmpty();
-  req.checkBody('lastname', 'Last name is required').notEmpty();
-  req.checkBody('username', 'Username is required').notEmpty();
-  req.checkBody('email', 'Email is required').notEmpty();
-  req.checkBody('password', 'Password is required').notEmpty();
-  req.checkBody('pwconfirm', 'Please confirm password').notEmpty(); // TODO Validate confirm password = password
-
-  var errors = req.validationErrors();
-  if(errors) {
-    res.render('adduser', {
-      errors: errors
-    });
-  } 
-  
-  else {
-  // hash encrypts the password and stores the hash + salt
-    var hash = bcrypt.hashSync(req.body.password, 10);
-    let post = {
-      first_name: req.body.firstname,
-      last_name: req.body.lastname,
-      username: req.body.username,
-      email: req.body.email,
-      password: hash,
-      accesslevel: req.body.accessrole
-    };
-    let sql = 'INSERT INTO users SET ?';
-    let query = connection.query(sql, post, (err, result) => {
-      console.log(result);
-    }); 
-  };
-});
+app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -152,5 +49,6 @@ app.use(function(err, req, res, next) {
 app.listen(3000, () => {
   console.log('Server started on localhost:3000');
 });
+//DELETE THIS WHEN DEPLOYING TO EB
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-// Routes should be in one file??
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/login');
 });
 
-router.get('/routetest', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login', (req, res, next) => {
+  res.render('login');
 });
 
 module.exports = router;
