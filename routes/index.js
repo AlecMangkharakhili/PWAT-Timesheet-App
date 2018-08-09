2050e51f84a90c444dc8752cb5307341997ae026
AlecMangkharakhili <alecmang097@gmail.com>
2018-08-09 13:15:14 -0500

    Added employee version of entry viewing

--- routes/index.js
+++ routes/index.js
@@ -59,9 +59,45 @@ router.post('/login', (req, res) => {
 
 router.get('/home', checkLoggedIn(), (req, res, next) => {
   var rowName = ['Name', 'Job', 'Date', 'Class Description', 'Bonus', '# of Seats', 'Tip', 'Hours Worked', 'Comments'];
-  if (req.user.accesslevel = 1)
+  if (req.user.accesslevel == 1)
   {
     db.query('SELECT users.name, timesheet.job_id, timesheet.date, timesheet.class_desc, timesheet.bonus, timesheet.num_seats, timesheet.tip, timesheet.hrs_worked, timesheet.comments FROM timesheet JOIN users ON timesheet.employee_id = users.employee_id ORDER BY date DESC;', (err, results) => {
+      var rowArr = [];
+      var timesheetArr = [];
+      for (let i = 0; i < results.length; i++)
+        {
+          let dateFmat = new Date(results[i].date);
+          dateFmat = dateFmat.toISOString().substring(0, 10);
+          rowArr.push(results[i].name);
+          rowArr.push(results[i].job_id);
+          rowArr.push(dateFmat);
+          rowArr.push(results[i].class_desc);
+          if (results[i].bonus == "NULL")
+          {
+            rowArr.push("None");
+          }
+          else
+          {
+            rowArr.push(results[i].bonus);
+          }
+          rowArr.push(results[i].num_seats);
+          rowArr.push('$' + results[i].tip);
+          rowArr.push(results[i].hrs_worked);
+          rowArr.push(results[i].comments);
+          timesheetArr.push(rowArr);
+          rowArr = [];
+        }
+      res.render('home', {
+        isManager: req.user.accesslevel,
+        sidebarName: (req.user.name),
+        timesheetRows: rowName,
+        timesheetData: timesheetArr
+      });
+    });
+  }
+  if (req.user.accesslevel == 0 )
+  {
+    db.query('SELECT users.name, timesheet.job_id, timesheet.date, timesheet.class_desc, timesheet.bonus, timesheet.num_seats, timesheet.tip, timesheet.hrs_worked, timesheet.comments FROM timesheet JOIN users ON timesheet.employee_id = users.employee_id WHERE users.name = ? ORDER BY date DESC;', req.user.name, (err, results) => {
       var rowArr = [];
       var timesheetArr = [];
       for (let i = 0; i < results.length; i++)
@@ -112,6 +148,8 @@ router.post('/entrydelete', checkLoggedIn(), (req, res, next) => {
       // CANNOT DELETE ENTRIES WITH NULL VALUES (I THINK)
       // '' NOT INTERPRETED AS null
       // MUST USE IS NULL OR IS NOT NULL
+      // FORGOT TO PUSH CHANGES FROM MAIN COMPUTER
+      // THIS IS FIXED
       db.query('DELETE FROM timesheet WHERE employee_id = ? AND job_id = ? AND date = ? AND class_desc = ? AND hrs_worked = ?', params, (err, res) => {
         console.log(err);
         console.log(res);
